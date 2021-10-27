f925c774ff9e89b3061f4e658df4a1f8057941a8
Alessandro Lumino <44137975+lumander@users.noreply.github.com>
2021-10-27 12:39:23 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Codemotion Example App - Canary Version!!
+          Welcome to Codemotion Example App!
         </h1>
 
         <p className={styles.description}>
