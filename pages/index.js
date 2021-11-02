c65892c36db8d0bc62582e7907a84e755ddbd254
lumander <alessandro.lumino@gmail.com>
2021-11-02 15:37:19 +0100

    change color to red

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title} style="color:blue">
+        <h1 className={styles.title}>
           Welcome to Codemotion Example App
         </h1>
 
