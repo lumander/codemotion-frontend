80202154d74c159dd763ae988bdd716d3ca32efb
Alessandro Lumino <44137975+lumander@users.noreply.github.com>
2021-11-02 13:16:04 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Codemotion Example App!
+          Welcome to Codemotion Example App - Refactor branch!
         </h1>
 
         <p className={styles.description}>
